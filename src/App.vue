bb6061360cb12b04e02769a760e889a80dbf026d
juacompe <juacompe@gmail.com>
2018-05-03 18:45:21 +0700

    Change title

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <span>Perlclip PWA</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <img src="./assets/logo.png" alt="Perlclip PWA">
       <router-view></router-view>
     </main>
   </div>
