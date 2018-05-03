887b90fb2233a03f8f2b58e7738a56cfb9d55631
juacompe <juacompe@gmail.com>
2018-05-03 15:13:25 +0700

    Finish counterstring function

--- src/models/Counterstring.js
+++ src/models/Counterstring.js
@@ -1,14 +1,10 @@
 export default {
   get (i) {
-    if (i === 3) {
-      return '*3*'
-    }
-    if (i === 2) {
-      return '2*'
-    }
-    if (i === 1) {
-      return '*'
-    }
-    return ''
+    if (i > 1) return this.previous(i) + '*'
+    if (i === 1) return '*'
+    else return ''
+  },
+  previous (i) {
+    return this.get(i - String(i).length - 1) + i
   }
 }
--- test/unit/specs/models/Counterstring.spec.js
+++ test/unit/specs/models/Counterstring.spec.js
@@ -2,17 +2,25 @@ import Counterstring from '@/models/Counterstring'
 
 describe('Counterstring.js', () => {
   describe('get', () => {
-    it('should produce an empty string for 0', () => {
+    it('returns an empty string for 0', () => {
       expect(Counterstring.get(0)).to.equal('')
     })
-    it('should produce a special string according to the given lenght', () => {
+    it('returns a special string according to the given lenght', () => {
       expect(Counterstring.get(1).length).to.equal(1)
     })
-    it('an asterisk is at a position in the string equal to the number that precedes it for even number', () => {
-      expect(Counterstring.get(2)).to.equal('2*')
-    })
-    it('an asterisk is at a position in the string equal to the number that precedes it for odds number', () => {
-      expect(Counterstring.get(3)).to.equal('*3*')
+    describe('each asterisk is at a position in the string equal to the number that precedes it', () => {
+      it('for small even number', () => {
+        expect(Counterstring.get(2)).to.equal('2*')
+      })
+      it('for small odds number', () => {
+        expect(Counterstring.get(3)).to.equal('*3*')
+      })
+      it('for 2-digit number', () => {
+        expect(Counterstring.get(10)).to.equal('*3*5*7*10*')
+      })
+      it('for 3-digit number', () => {
+        expect(Counterstring.get(100)).to.equal('*3*5*7*9*12*15*18*21*24*27*30*33*36*39*42*45*48*51*54*57*60*63*66*69*72*75*78*81*84*87*90*93*96*100*')
+      })
     })
   })
 })
