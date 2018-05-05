67e2adb75ea51530f266be08d52343b46279bdfb
juacompe <juacompe@gmail.com>
2018-05-05 15:23:05 +0700

    Support form submit

--- src/components/Counterstring.vue
+++ src/components/Counterstring.vue
@@ -3,9 +3,9 @@
     <div class="mdl-cell mdl-cell--2-col mdl-cell mdl-cell--1-col-tablet mdl-cell--hide-phone"></div>
     <div class="mdl-cell mdl-cell--8-col">
       <h2>Counterstring</h2>
-      <form>
+      <form v-on:submit.prevent="get">
         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
-          <input id="length" type="number" v-model="length" v-on:click="clearLength()" class="mdl-textfield__input"/>
+          <input id="length" type="number" v-model="length" v-on:click="clearLength" class="mdl-textfield__input"/>
         </div>        
         <div>
           <a @click.prevent="get"  class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
