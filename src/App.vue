f41fc8b9557eaa9ddefba396b57b97d9f7e2abeb
juacompe <juacompe@gmail.com>
2018-05-10 18:30:21 +0700

    Put odd-e logo back in about

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
   <div id="app" class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-      <span class="mdl-layout-title">Perlclip PWA</span>
+        <span class="mdl-layout-title">Perlclip PWA</span>
       </div>
     </header>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">Perlclip PWA</span>
+      <nav class="mdl-navigation">
+        <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Counterstring</router-link>
+        <router-link class="mdl-navigation__link" to="/about" @click.native="hideMenu">About</router-link>
+      </nav>
+    </div>
     <main class="mdl-layout__content">
       <div class="page-content">
       <router-view></router-view>
@@ -14,15 +21,13 @@
 </template>
 
 <script>
+require('material-design-lite')
 export default {
   name: 'app'
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
-@import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
-
 body {
   margin: 0;
 }
@@ -57,4 +62,7 @@ header span {
   box-sizing: border-box;
   padding-top: 16px;
 }
+
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <img class="mdl-cell mdl-cell--2-col" src="../assets/logo.png" alt="Perlclip PWA">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
